73b8f923d9da7b44930d241634c5d61d6137fba9
Fam. Ott <Fam. Ott>
2016-12-08 19:58:21 +0100

    GREEN

---
+++ CalculatorTestProject/Calculator/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Controller
+    {
+
+        public bool Do()
+        {
+            return true;
+        }
+    }
+}
